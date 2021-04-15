100cbd3916f40b0a0de052400e2f584cfe8f7f07
Ty <qwl@guopika.com>
2021-04-15 12:07:09 +0800

    .

---
+++ src/IService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace XinMo\Search;
+
+interface IService
+{
+    public static function insert();
+
+    public static function get();
+
+    public static function update();
+
+    public static function delete();
+}
